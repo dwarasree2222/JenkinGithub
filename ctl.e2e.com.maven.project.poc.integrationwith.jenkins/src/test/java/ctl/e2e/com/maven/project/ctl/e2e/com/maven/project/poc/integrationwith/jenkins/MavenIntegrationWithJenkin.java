63f4a5c46be3c97631962278a3443da0d2df4f69
ab72386 <srikanth.dwara@centurylink.com>
2017-08-02 22:38:11 -0600

    initial commit

---
+++ ctl.e2e.com.maven.project.poc.integrationwith.jenkins/src/test/java/ctl/e2e/com/maven/project/ctl/e2e/com/maven/project/poc/integrationwith/jenkins/MavenIntegrationWithJenkin.java
@@ -0,0 +1,12 @@
+package ctl.e2e.com.maven.project.ctl.e2e.com.maven.project.poc.integrationwith.jenkins;
+
+import org.testng.annotations.Test;
+
+public class MavenIntegrationWithJenkin {
+	@Test
+	public void sampleMavenJenkinMethod()
+	{
+		System.out.println("Hello sample Maven Jenkin Integration");
+	}
+
+}
